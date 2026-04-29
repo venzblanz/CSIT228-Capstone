fd595c9218513d7329ff8fdbbf3d11fd6b1c9228
Venz Virni Blanza <venzblanz@gmail.com>
2026-04-29 22:41:37 +0800

    forgot to push this one

--- src/main/java/com/javafx/csit228capstone/helper/MenuController.java
+++ src/main/java/com/javafx/csit228capstone/helper/MenuController.java
@@ -1,5 +1,6 @@
 package com.javafx.csit228capstone.helper;
 
+import com.javafx.csit228capstone.utils.SceneNavigator;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -21,10 +22,13 @@ public class MenuController {
     @FXML private Button accountBtn;
     @FXML private HBox logoutBtn;
 
+    private final SceneNavigator sceneNavigator = SceneNavigator.getInstance();
+
     public Button getQueueBtn() { return queueBtn; }
     public Button getDashboardBtn() { return dashboardBtn; }
     public Button getScheduleBtn() { return scheduleBtn; }
     public Button getAccountBtn() { return accountBtn; }
+
 
     @FXML
     public void initialize(){
@@ -51,55 +55,17 @@ public class MenuController {
     }
 
     public void goToQueue() {
-        try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javafx/csit228capstone/queue/queue.fxml"));
-            Stage stage = (Stage) queueBtn.getScene().getWindow();
-            System.out.println(stage.getWidth() + " | "  + stage.getHeight());
-
-            double width = stage.getWidth() - 16.00;
-            double height = stage.getHeight() - 39.00;
-            boolean wasMaximized = stage.isMaximized();
-
-            Scene scene = new Scene(loader.load(), width, height);
-            scene.getStylesheets().add(getClass().getResource("/styles/queue.css").toExternalForm());
-            stage.setScene(scene);
-            if (wasMaximized) {
-                stage.setMaximized(true);
-            }
-            stage.setResizable(true);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        sceneNavigator.navigate("/com/javafx/csit228capstone/queue/queue.fxml", queueBtn,"/styles/queue.css");
     }
     public void goToDashboard(){
-        try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/javafx/csit228capstone/dashboard.fxml"));
-            Stage stage = (Stage) dashboardBtn.getScene().getWindow();
-            System.out.println(stage.getWidth() + " | "  + stage.getHeight());
-
-            double width = stage.getWidth() - 16.00;
-            double height = stage.getHeight() - 39.00;
-            boolean wasMaximized = stage.isMaximized();
-
-            Scene scene = new Scene(loader.load(), width, height);
-            scene.getStylesheets().add(getClass().getResource("/styles/dashboard.css").toExternalForm());
-            stage.setScene(scene);
-            if (wasMaximized) {
-                stage.setMaximized(true);
-            }
-            stage.setResizable(true);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        sceneNavigator.navigate("/com/javafx/csit228capstone/dashboard.fxml", queueBtn,"/styles/dashboard.css");
     }
     public void setActiveButton(Button active) {
-        // reset all first
         dashboardBtn.setStyle("");
         queueBtn.setStyle("");
         scheduleBtn.setStyle("");
         accountBtn.setStyle("");
 
-        // highlight the active one
         active.setStyle("-fx-background-color: #218AD5; -fx-text-fill: white; -fx-style: bold;");
     }
 }
