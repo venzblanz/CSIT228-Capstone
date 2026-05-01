bdfc13b93b8b2fc32441a4c36647c291ddea71f0
EMERALD <reaemerald2006@gmail.com>
2026-05-01 16:28:37 +0800

    Add functionality to filters

--- src/main/java/com/javafx/csit228capstone/screens/patient_records/PatientRecordsController.java
+++ src/main/java/com/javafx/csit228capstone/screens/patient_records/PatientRecordsController.java
@@ -1,6 +1,7 @@
 package com.javafx.csit228capstone.screens.patient_records;
 
 import com.javafx.csit228capstone.model.QueueHistory;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -17,23 +18,172 @@ public class PatientRecordsController {
     @FXML private TableColumn<QueueHistory, String> dateColumn;
     @FXML private TableColumn<QueueHistory, String> staffColumn;
 
-    @FXML private MenuButton serviceFilter;
+    @FXML private MenuButton departmentFilter;
     @FXML private MenuButton statusFilter;
     @FXML private CheckMenuItem pendingStatus;
     @FXML private CheckMenuItem completedStatus;
     @FXML private CheckMenuItem cancelledStatus;
+    @FXML private CheckMenuItem allStatus;
+    @FXML private CheckMenuItem genWellnessDept;
+    @FXML private CheckMenuItem womenHealthDept;
+    @FXML private CheckMenuItem specialFieldsDept;
+    @FXML private CheckMenuItem diagnosticsLabDept;
+    @FXML private CheckMenuItem allDept;
     @FXML private DatePicker dateFromFilter;
     @FXML private DatePicker dateToFilter;
 
     @FXML private Button loadHistoryBtn;
 
+    private boolean updating;
+
     public void initialize() {
-        LocalDate selectedDateFrom = dateFromFilter.getValue();
-        LocalDate selectedDateTo = dateToFilter.getValue();
+        initializeDatePicker();
+        initializeFilters();
+        initializeTable();
+    }
+
+    @FXML private void loadHistory() {
+
+    }
+
+    private void initializeDatePicker() {
+        dateFromFilter.valueProperty().addListener((obs, oldDate, newDate) -> {
+            if (newDate != null) {
+                dateToFilter.setDayCellFactory(picker -> new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (empty || item == null) {
+                            return;
+                        }
+
+                        if (item.isBefore(newDate)) {
+                            setDisable(true);
+                            setStyle("-fx-background-color: #ffc0cb;");
+                        }
+                    }
+                });
+
+                if (dateToFilter.getValue() != null &&
+                        dateToFilter.getValue().isBefore(newDate)) {
+                    dateToFilter.setValue(null);
+                }
+            }
+        });
+
+        dateToFilter.valueProperty().addListener((obs, oldDate, newDate) -> {
+            if (newDate != null) {
+                dateFromFilter.setDayCellFactory(picker -> new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (empty || item == null) return;
+
+                        if (item.isAfter(newDate)) {
+                            setDisable(true);
+                        }
+                    }
+                });
+
+                if (dateFromFilter.getValue() != null &&
+                        dateFromFilter.getValue().isAfter(newDate)) {
+                    dateFromFilter.setValue(null);
+                }
+            }
+        });
+    }
+
+    private void initializeFilters() {
+        allDept.setSelected(true);
+
+        genWellnessDept.setSelected(true);
+        womenHealthDept.setSelected(true);
+        specialFieldsDept.setSelected(true);
+        diagnosticsLabDept.setSelected(true);
+
+        updateMenuText("Select Department", departmentFilter);
+
+        allStatus.setSelected(true);
+
+        pendingStatus.setSelected(true);
+        completedStatus.setSelected(true);
+        cancelledStatus.setSelected(true);
+
+        updateMenuText("Select Status", statusFilter);
 
         pendingStatus.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Status", statusFilter));
         completedStatus.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Status", statusFilter));
         cancelledStatus.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Status", statusFilter));
+        allStatus.selectedProperty().addListener((obs, oldVal, newVal) -> {
+
+            if (updating) return;
+            updating = true;
+
+            pendingStatus.setSelected(newVal);
+            completedStatus.setSelected(newVal);
+            cancelledStatus.setSelected(newVal);
+
+            updating = false;
+
+            updateMenuText("Select Status", statusFilter);
+        });
+        ChangeListener<Boolean> syncAll = (obs, oldVal, newVal) -> {
+            if (updating) return;
+            updating = true;
+            boolean allSelected = pendingStatus.isSelected() && completedStatus.isSelected() && cancelledStatus.isSelected();
+            allStatus.setSelected(allSelected);
+
+            updating = false;
+
+            updateMenuText("Select Status", statusFilter);
+        };
+
+        pendingStatus.selectedProperty().addListener(syncAll);
+        completedStatus.selectedProperty().addListener(syncAll);
+        cancelledStatus.selectedProperty().addListener(syncAll);
+
+
+        genWellnessDept.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Department", departmentFilter));
+        womenHealthDept.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Department", departmentFilter));
+        specialFieldsDept.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Department", departmentFilter));
+        diagnosticsLabDept.selectedProperty().addListener((obs, oldVal, newVal) -> updateMenuText("Select Department", departmentFilter));
+        allDept.selectedProperty().addListener((obs, oldVal, newVal) -> {
+
+            if (updating) return;
+            updating = true;
+
+            genWellnessDept.setSelected(newVal);
+            womenHealthDept.setSelected(newVal);
+            specialFieldsDept.setSelected(newVal);
+            diagnosticsLabDept.setSelected(newVal);
+
+            updating = false;
+
+            updateMenuText("Select Department", departmentFilter);
+        });
+        ChangeListener<Boolean> syncAllDept = (obs, oldVal, newVal) -> {
+
+            if (updating) return;
+            updating = true;
+
+            boolean allSelected = genWellnessDept.isSelected() && womenHealthDept.isSelected() && specialFieldsDept.isSelected() && diagnosticsLabDept.isSelected();
+            allDept.setSelected(allSelected);
+
+            updating = false;
+
+            updateMenuText("Select Department", departmentFilter);
+        };
+
+        genWellnessDept.selectedProperty().addListener(syncAllDept);
+        womenHealthDept.selectedProperty().addListener(syncAllDept);
+        specialFieldsDept.selectedProperty().addListener(syncAllDept);
+        diagnosticsLabDept.selectedProperty().addListener(syncAllDept);
+    }
+
+    private void initializeTable() {
+        queueTable.getColumns().forEach(col -> col.setReorderable(false));
     }
 
     private void updateMenuText(String text, MenuButton menuButton) {
@@ -41,15 +191,22 @@ public class PatientRecordsController {
         List<String> selected = new ArrayList<>();
 
         for (MenuItem item : menuButton.getItems()) {
-            if (item instanceof CheckMenuItem cmi && cmi.isSelected()) {
-                selected.add(cmi.getText());
+            if (item instanceof CheckMenuItem cmi) {
+                if (cmi.isSelected()) {
+                    selected.add(cmi.getText());
+                }
             }
         }
 
         if (selected.isEmpty()) {
             menuButton.setText(text);
-        } else {
-            menuButton.setText(String.join(", ", selected));
+            return;
         }
+        if ((menuButton == departmentFilter && allDept.isSelected()) || (menuButton == statusFilter && allStatus.isSelected())) {
+            menuButton.setText("All");
+            return;
+        }
+
+        menuButton.setText(String.join(", ", selected));
     }
 }
