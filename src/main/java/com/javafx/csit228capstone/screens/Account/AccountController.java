bd13ec3cb2e58c6e0ca844fe4f166d77e74d7f40
sariiie <sarahmaebasalo8@gmail.com>
2026-05-05 12:57:14 +0800

    added more screen(account)

--- src/main/java/com/javafx/csit228capstone/screens/Account/AccountController.java
+++ src/main/java/com/javafx/csit228capstone/screens/Account/AccountController.java
@@ -25,21 +25,15 @@ import java.util.ResourceBundle;
 
 public class AccountController implements Initializable {
 
-    // Profile labels
     @FXML private Label profileNameLabel;
     @FXML private Label profileIdLabel;
 
-    // Manage rows
     @FXML private HBox patientRecordsRow;
     @FXML private HBox myAppointmentsRow;
     @FXML private HBox queueStatusRow;
-
-    // Settings & Privacy rows
-    @FXML private HBox notificationSettingsRow;
+   // @FXML private HBox notificationSettingsRow;
     @FXML private HBox securityRow;
     @FXML private Button editBtn;
-
-    // Others rows
     @FXML private HBox supportRow;
     @FXML private HBox termsRow;
     @FXML private HBox aboutRow;
@@ -54,13 +48,33 @@ public class AccountController implements Initializable {
         // profileIdLabel.setText(SessionManager.getUser().getPatientId());
 
         editBtn.setOnAction(e -> onEdit());
+//        patientRecordsRow.setOnMouseClicked(e -> onPatientRecords());
+//        queueStatusRow.setOnMouseClicked(e -> onQueueStatus());
+//        myAppointmentsRow.setOnMouseClicked(e -> onMyAppointments());
+        securityRow.setOnMouseClicked(e -> onSecurityPrivacy());
+        termsRow.setOnMouseClicked(e -> onTermsAndConditions());
+        aboutRow.setOnMouseClicked(e -> onAboutMedServe());
     }
 
     private void onEdit() {
         handleEdit();
     }
 
+    private void onPatientRecords() {
+        handlePatientRecords();
+    }
 
+    private void onMyAppointments() {
+        handleMyAppointments();
+    }
+
+    private void onQueueStatus() {
+        handleCheckQueueStatus();
+    }
+
+    private void onSecurityPrivacy() {
+        handleSecurityPrivacy();
+    }
 
     private void handlePatientRecords() {
 
@@ -80,23 +94,33 @@ public class AccountController implements Initializable {
     }
 
     private void handleSecurityPrivacy() {
-
+        sceneNavigator.navigate(
+                "/com/javafx/csit228capstone/account/security_privacy.fxml", securityRow, "/styles/account.css"
+        );
     }
-
-    // ── Others ───────────────────────────────────────────────────────────────
 
     private void handleContactSupport() {
 
     }
 
     private void handleTermsAndConditions() {
-
+        sceneNavigator.navigate(
+                "/com/javafx/csit228capstone/account/terms_condition.fxml", termsRow, "/styles/account.css"
+        );
+    }
+    private void onTermsAndConditions() {
+        handleTermsAndConditions();
     }
 
     private void handleAboutMedServe() {
-
-
+        sceneNavigator.navigate(
+                "/com/javafx/csit228capstone/account/about_medserve.fxml", aboutRow, "/styles/account.css"
+        );
     }
+    private void onAboutMedServe() {
+        handleAboutMedServe();
+    }
+
 
     @FXML
     private void handleEdit() {
